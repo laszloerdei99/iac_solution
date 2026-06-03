62a7934b02a7e8633d4fd261ed617d767a4120d0
Erdei László <78907153+laszloerdei99@users.noreply.github.com>
2026-06-03 02:41:13 +0200

    Add files via upload

---
+++ configmap.tf
@@ -0,0 +1,21 @@
+resource "kubernetes_config_map_v1" "ocr_services_config" {
+  metadata {
+    name      = "ocr-services-config"
+    namespace = var.namespace
+  }
+
+  data = {
+    REDIS_HOST              = "redis"
+    REDIS_PORT              = "6379"
+    MINIO_HOST              = "minio:9000"
+    MINIO_ACCESS_KEY        = "minioadmin"
+    MINIO_SECRET_KEY        = "minioadmin"
+    MINIO_BUCKET            = "ocr-processing"
+    RABBITMQ_HOST           = "rabbitmq"
+    RABBITMQ_USER           = "guest"
+    RABBITMQ_PASSWORD       = "guest"
+    KAFKA_BOOTSTRAP_SERVERS = "kafka:9092"
+    KAFKA_TOPIC             = "ocr_results"
+    KAFKA_GROUP_ID          = "text-aggregators"
+  }
+}
